df8c583ea76439920b97fb8dc53b46721e3ac929
Ain Chung <artnouveau0804@gmail.com>
2022-03-02 15:38:12 +0900

    #2.3 const & let
    1. JS에는 변수를 만드는 방법이 2가지다: const, let
    1)const: 값이 변하지 않는 상수를 선언할 때 ex:가끔 변수를 업데이트 하고자 할 때 값을 바꾸고 싶을 때가 있음. 이럴땐 const를 쓰면 오류가 남.
    2)let: let은 값을 변화시킬 수 있음 ->값을 변화 시킬 때 아래에 적는데, 이때는 let을 또 다시 적지 않아도 됨. 처음에 선언 할때는 let myName = “ain”; 이런식 이였지만 myName이 이미 생성 됐고 업데이트만 하려면 let만 빼면 됨. 그런 후 이 업데이트 아랫줄부터 업데이트가 된 변수.(만약 const로 업데이트를 하려고 하면 console에서 오류가 남.)
    2. 그래서 첫줄에 const와 let만 봐도 “아 얘는 나중에 업데이트 되겠구나” “얘는 처음부터 끝까지 이 상수겠구나”를 알 수 있음
    3. 대부분 기본적으로 const를 쓰고 업데이트를 해야하는 변수가 있을 때만(필요할 때만) let을 씀.
    4. const와 let의 옛날 버전은 var인데 브라우저가 이해는 하지만 모든 변수를 업데이트 할 수 있어서 우리가 값을 잘못 적어도 알려주지 않음/ 보호해주지 않음.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const a = 6;
 const b = 2;
-const myName = "ain";
+let myName = "ain";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+console.log("hello " + myName);
+
+myName = "emily";
 console.log("hello " + myName);
