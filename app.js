61ef01a9ef1676e452900b8f74989d3d5f98e1e6
Ain Chung <artnouveau0804@gmail.com>
2022-03-04 19:01:41 +0900

    #2.8 functions (part.2)
    1.
    function sayHello(nameOfPerson){
    console.log(nameOfPerson)
    }
    
    sayHello(“monica”)
    sayHello(“rachel”)
    sayHello(“pheobe”)
    ==monica
    rachel
    pheobe
    nameOfPerson이 variable이고 그 variable의 값을 각각 sayHello()괄호 안에 적어야 함. 그러면 각각의 값이 출력됨.
    
    variable은 여러개를 넣을 수 있음.
    ex)
    function sayHello(nameOfPerson, age) {
    console.log(“hello my name is ” + nameOfPerson + “ and I am ” + age)
    };
    sayHello(“monica”, 25);
    sayHello(“rachel”, 25);
    sayHello(“pheobe”, 26);
    ==hello my name is monica and I am 25
    hello my name is rachel and I am 25
    hello my name is pheobe and I am 26
    *만약 sayHello()괄호에 아무것도 넣지 않는다면 undefined가 뜸.
    
    2.다른 예:
    ex) function plus(a,b) {
    console.log(a + b);
    }
    plus();
    이상태로 출력된다면 NaN가 뜨는데 이것은 Not a Number라는 뜻.
    -()괄호에 들어가는 값을 argument라고 함.
    -a와 b의 자리는 변수이기 때문에 아무이름이나 지어도 됨.
    -order(순서)는 중요함.
    -만약 console.log(argument)을 function variable(argument)의 바깥에서 한다면 error가 발생함. 왜냐하면 바깥에서는 function 안에 있는 argument들의 영향을 받지 않기 때문.
    
    3. function은 object안에서도 가능.
    const player = {
     name: “monica”,
     sayHello: function(otherPersonsName) {
      console.log(“hello my name is ” + otherPersonsName + “, nice to meet you.”);
     },
    };
    
    console.log(player.name);
    player.sayHello(”rachel”);
    player.sayHello(”ross”);
    player.sayHello(”pheobe”);
    player.sayHello(”chandler”);
    ==monica
    hello my name is rachel, nice to meet you.
    hello my name is ross, nice to meet you.
    hello my name is pheobe, nice to meet you.
    hello my name is chandler, nice to meet you.

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
-function sayHello() {
-  console.log("hello my name is");
+//function ex.1
+function sayHello(nameOfPerson, age, hobby) {
+  console.log(
+    "hello my name is " +
+      nameOfPerson +
+      " and I am " +
+      age +
+      " and I like " +
+      hobby
+  );
 }
 
-sayHello();
+sayHello("monica", 25, "cooking");
+sayHello("rachel", 25, "fashions");
+sayHello("pheobe", 27, "walking");
+
+//function ex.2 (calculator)
+function plus(a, b) {
+  console.log(a + b);
+}
+
+plus(5, 5);
+
+function devide(a, b) {
+  console.log(a / b);
+}
+devide(6, 3);
+
+//object안에서 function
+const player = {
+  name: "monica",
+  sayHello: function (otherPersonsName) {
+    console.log("hello " + otherPersonsName + ", nice to meet you.");
+  },
+};
+
+console.log(player.name);
+player.sayHello("rachel");
+player.sayHello("ross");
+player.sayHello("pheobe");
+player.sayHello("chandler");
