cda73b845da5ed131583093013a5753907f93e1d
Ain Chung <artnouveau0804@gmail.com>
2022-03-04 18:03:15 +0900

    #2.7 functions (part.1)
    hello my name is monica
    hello my name is rachel
    hello my name is pheobe
    여기서 “hello my name is”는 복붙하고 이름부분만 바꾸고 싶을 때,
    console.log(“hello my name is monica”);
    console.log(“hello my name is rachel”);
    console.log(“hello my name is pheobe”);
    이런식으로 하면 손이 많이 감.
    
    이름부분만 바꾸려면 function 기능을 쓰면 좋음.
    1. 우선 function sayHello(){
     console.log(“hello my name is”);
    }
    sayHello();
    sayHello();
    sayHello();
    == hello my name is
    hello my name is
    hello my name is
    
    sayHello를 치면 “hello my name is”가 출력 되게 하는 기능
    2. 여기서 sayHello(monica)라고 쳐도 똑같이 “hello my name is”가 출력됨.

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-const player = {
-  name: "monica",
-  age: 25,
-  fat: true,
-};
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
+function sayHello() {
+  console.log("hello my name is");
+}
 
-player.fat = false;
-console.log(player);
-
-player.lastName = "geller";
-console.log(player);
-
-player.age = player.age + 10;
-console.log(player.age);
+sayHello();
