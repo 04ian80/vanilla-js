2b8979dbba7707552d7345deff5a0cdfce371090
Ain Chung <artnouveau0804@gmail.com>
2022-03-05 23:43:40 +0900

    #2.10 challenge
    object안에 function으로 calculator 만들기

--- app.js
+++ app.js
@@ -1,74 +1,19 @@
-//function ex.1
-function sayHello(nameOfPerson, age, hobby) {
-  console.log(
-    "hello my name is " +
-      nameOfPerson +
-      " and I am " +
-      age +
-      " and I like " +
-      hobby
-  );
-}
-
-sayHello("monica", 25, "cooking");
-sayHello("rachel", 25, "fashions");
-sayHello("pheobe", 27, "walking");
-
-//function ex.2 (calculator)
-function plus(a, b) {
-  console.log(a + b);
-}
-
-plus(5, 5);
-
-function devide(a, b) {
-  console.log(a / b);
-}
-devide(6, 3);
-
-//object안에서 function
-const player = {
-  name: "monica",
-  sayHello: function (otherPersonsName) {
-    console.log("hello " + otherPersonsName + ", nice to meet you.");
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  powerOf: function (a, b) {
+    console.log(a ** b);
   },
 };
 
-console.log(player.name);
-player.sayHello("rachel");
-player.sayHello("ross");
-player.sayHello("pheobe");
-player.sayHello("chandler");
-
-//연습
-const friends = {
-  name: "monica",
-  age: 25,
-  job: "head chef",
-};
-
-console.log(friends.name);
-console.log(friends);
-
-function describe(fruits, color) {
-  console.log(fruits + "'s color is " + color);
-}
-
-describe("apple", "red");
-describe("grape", "purple");
-describe("banana", "yellow");
-describe("watermelon", "green and black");
-
-const object = {
-  name: "monica",
-  intro: function (fruits, color) {
-    console.log(fruits + "'s color is " + color);
-  },
-};
-console.log(object.name);
-object.intro("banana", "yellow");
-
-const toDo = ["walk the dog", "make lunch", "do the homework"];
-console.log(toDo[1]);
-toDo[1] = "watering the plant";
-console.log(toDo[1]);
+calculator.add(2, 3);
+calculator.minus(5, 34);
+calculator.divide(100, 2);
+calculator.powerOf(2, 3);
