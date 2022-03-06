0ec110a1e2e88f59385a7931b0b3748b15bd804a
Ain Chung <artnouveau0804@gmail.com>
2022-03-06 19:38:39 +0900

    #2.11 returns

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const calculator = {
-  add: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  divide: function (a, b) {
-    console.log(a / b);
-  },
-  powerOf: function (a, b) {
-    console.log(a ** b);
-  },
-};
+const age = 23;
+function calculateFrAge(ageOfKr) {
+  return ageOfKr - 2;
+}
 
-calculator.add(2, 3);
-calculator.minus(5, 34);
-calculator.divide(100, 2);
-calculator.powerOf(2, 3);
+const frAge = calculateFrAge(age);
+console.log(frAge);
