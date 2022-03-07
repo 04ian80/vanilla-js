7f71836e4a6bfe913ffff6a4d4c3a0bcb12a7edd
Ain Chung <artnouveau0804@gmail.com>
2022-03-08 00:05:12 +0900

    생활코딩 JS-21 (while)
    while반복문
    
    ex)
    let i = 0;
    while (i < 3) {
     document.write(“<li>2</li>”)
     document.write(“<li>3</li>”)
    i = i + 1;
    }
    해석: 변수 i는 0일 때, 0은 3보다 작아서 true. 0 + 1 = 1이며 한번 반복,
    i 가 1이 됐을 때, 1은 3보다 작아서 true. 1 + 1 = 2이며 두번째 반복,
    i 가 2가 됐을 때, 2는 3보다 작아서 true. 2 + 1 = 3으로 3 < 3은 만들 수 없기 때문에 false로 이어짐. 이렇게 해서 3번 반복에서 끝낼 수 있음.

---
+++ shcd/shcd.js
@@ -0,0 +1,17 @@
+//array & push & length
+const snacks = ["choco chip", "poca chip"];
+snacks.push("ice-cream");
+document.write(snacks);
+document.write(snacks[1]);
+document.write(snacks.length);
+
+//while 반복문
+document.write("<li>1</li>");
+let i = 0;
+while (i < 3) {
+  document.write("<li>2</li>");
+  document.write("<li>3</li>");
+  i = i + 1;
+}
+document.write("<li>4</li>");
+document.write("<li>5</li>");
