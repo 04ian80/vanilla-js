69182ce92ab5568e1b2c3cb6f23ae14bf72e4ae6
Ain Chung <artnouveau0804@gmail.com>
2022-03-11 03:14:05 +0900

    생활코딩 JS-27 함수(return)
    1. 1+1은 숫자 2에 대한 표현식(expression)
    2. 1===1는 true라는 값에 대한 표현식
    3. return이란 반환하다.
    
    	function sum(a, b) {
    	 document.write(a + b);
    	};
    	sum(5, 6);
    이 함수는 한가지 기능, 즉 document.write()이란 기능 안에 갇혀서 다른 기능을 제대로 못 해냄.
    
    	function sum(a, b) {
    	 return a + b;
    	};
    	document.write(sum(1, 2));
    	console.log(sum(1, 2));
    이 함수는 a + b 기능을 하고 우리가 원하는 값을 이 기능을 거쳐 반환(return)해 줌으로 function바깥에서 여러가지 기능을 활용 할 수 있다. document.write외에도 console.log나 alert 등등…

--- shcd/shcd.js
+++ shcd/shcd.js
@@ -25,8 +25,15 @@ document.write("<li>1</li>");
 two();
 document.write("<li>3</li>");
 two();
-
+//매개변수와 인자(parameter & argument)
 function sum(a, b) {
   document.write(a + b);
 }
 sum(5, 6);
+
+function sum2(a, b) {
+  return a + b;
+}
+document.write("<br>" + sum2(1, 2));
+document.write("<div style='color: red'>" + sum2(1, 2) + "</div>");
+console.log(sum2(1, 2));
