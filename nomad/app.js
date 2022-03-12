54d1f96050180c7a786f0f0bb5690740c2768224
Ain Chung <artnouveau0804@gmail.com>
2022-03-12 20:29:12 +0900

    #2.11 returns
    1.
    외국나이를 계산 하는 함수를 만들 것:
    
    const age = 23;
    function calculateFrAge(ageOfKr) {
      return ageOfKr - 2;
    }
    
    const frAge = calculateFrAge(age);
    console.log(frAge);
    
    해석:
    1. age(내나이)라는 변수에 23 값을 넣어줌.
    2. calcualteFrAge(외국나이계산)라는 함수를 만들고 매개변수를 ageOfKr(한국나이)로 지정해줌.
    3. 그리고 그 함수의 기능은 calcualteFrAge() 를 입력하면,
    ageOfKr(한국나이) - 2를 실행시킬 것.
    4. frAge(외국나이)라는 변수를 입력하면 caculateFrAge(age)를 실행시킬 것.
    (age가 ageOfKr에 들어가 23-2가 됨)
    5. frAge를 console.log 입력하면  21가 출력됨.
    6. return을 안쓰면 결과가 출력이 안됨.
    -return을 써서 값을 받아왔기 때문에 calculateFrAge라는 함수는 return이 들어간 함수 바깥에서 return(반환) 한 값이 됨.
    
    -return은 값을 ‘get’하는 keyword.
    -function은 어떤 일을 수행하고 결과를 알려줌
    
    2.
    계산기 만들기 숙제:
    1. console.log를 함수 안에 넣으면 다른 기능을 못하기 때문에 대신 return을 넣는다.
    2. 그리고 값을 넣을 함수를 만든다.(ex: const plusResult = calculator.add(2, 3)).
    3. 서로 의존 할 수 있어서 값을 넣는 자리에 return된 함수이름을 넣을 수 있음.

--- nomad/app.js
+++ nomad/app.js
@@ -5,3 +5,27 @@ function calculateFrAge(ageOfKr) {
 
 const frAge = calculateFrAge(age);
 console.log(frAge);
+
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  power: function (a, b) {
+    return a ** b;
+  },
+  times: function (a, b) {
+    return a * b;
+  },
+};
+
+const plusResult = calculator.add(2, 3);
+const minusResult = calculator.minus(plusResult, 1);
+const divideResult = calculator.divide(36, minusResult);
+const powerResult = calculator.power(minusResult, 2);
+const timesResult = calculator.times(divideResult, powerResult);
