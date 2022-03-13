460251dc16ebd1f08c2061198128c68ed0b4e9d1
Ain Chung <artnouveau0804@gmail.com>
2022-03-13 23:28:55 +0900

    #2.12 conditionals
    조건문(if & else)
    
    음주가능 나이 계산하기:
    1. 변수를 바로 사용하지 않고 함수를 만들기:
    1) prompt
    - const age = prompt(“message”);
    prompt는 사용자에게 메시지 창을 띄울 수 있게 해줌.(메시지는 string이여야 함)
    그리고 console.log(age);를 출력하면 alert창에 message내용이 나오고 그 아래에 입력란이 생김. 거기에 입력하는 내용을 console에 띄워줌.
    단점 {
    - 하지만 이 코드는 굉장히 실용적이지 않고 html, css를 바꿀 수도 없음.
    - 우리가 입력란에 무언가를 입력하기 전까지는 계속 창이 띄워져 있음.
    - 만약 입력란에 무언가를 입력하지 않고 cancle 해버린다면 console에는 null이 뜨고, 아무것도 입력하지 않은 상태에서 ok를 누르면 console에 아무것도 없이 비어있음.
    }
    배울것:
    - prompt는 입력란에 뭘 쓰든 type이 string이기 때문에 숫자를 써도 “15” 이런식으로 string으로 변함.
    - string을 number로 바꾸려면 parseInt 키워드를 써야함(convert string to a number )
    - string을 number로 바꾸는 이유는 만약 사용자에게 나이를 물어봤는데 숫자가 아닌걸 입력했을 때 인식이 됨. 문자를 입력하면 console에는 NaN(Not a Number)이 뜸.
    그리고 어떤 수보다 큰지 작은지도 비교할 수 있음. string은 비교를 못함,
    1. console.log(typeof age);를 치면 age라는 변수의 타입이 무엇이냐는 뜻이고 alert 입력란에 숫자를 써도 string으로 인식함.
    2. parseInt()라는 function이 string을 number로 변환시켜줌.
    3. console.log()안에 parseInt(age)를 써주면 string을 number로 바꿔줌. 그리고 alert입력란에 숫자를 입력해주면 console창에는 타입이 숫자로 바뀌어서 출력이 됨.
    4. parseInt()는 함수이기 때문에 const age = parseInt(prompt(“How old are you?”));를 해주면  age라는 변수 안에 함수가 적용됨.
    - const age = parseInt(prompt(“How old are you?”));
    -> const age = parseInt(prompt(“15”));
    -> const age = 15;
    만약 string을 입력한다면 NaN가 뜸.

--- nomad/app.js
+++ nomad/app.js
@@ -1,33 +1,3 @@
-const age = 23;
-function calculateFrAge(ageOfKr) {
-  return ageOfKr - 2;
-}
+const age = parseInt(prompt("How old are you?"));
 
-const frAge = calculateFrAge(age);
-console.log(frAge);
-
-const calculator = {
-  add: function (a, b) {
-    console.log("hello");
-    return a + b;
-    console.log("bye");
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-};
-
-const plusResult = calculator.add(2, 3);
-const minusResult = calculator.minus(plusResult, 1);
-const divideResult = calculator.divide(36, minusResult);
-const powerResult = calculator.power(minusResult, 2);
-const timesResult = calculator.times(divideResult, powerResult);
+console.log(age);
