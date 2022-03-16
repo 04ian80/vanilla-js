bf65a8cecd13aaef8888681ba37e518ac4b4706d
Ain Chung <artnouveau0804@gmail.com>
2022-03-16 22:12:18 +0900

    #3.3 events (part.2)
    onclick, oncopy…
    =on은 listen이라는 뜻
    마우스가 title위에 있을 때 event는 [onmouseenter]
    
    function handleMouseEnter(){title.innerText = “mouse is here”;}
    title.addEventListner(“mouseenter”, handleMouseEnter);
    -해석: 함수 handleMouseEnter을 실행하면 inner text가 “mouse is here”이라는 문구로 바뀌어서 출력됨. “mouseenter” event를 실행시키기 위해 title.addEventListner() 괄호 안에 event이름과 함수 이름을 입력해줌.
    
    function handleMouseLeave(){title.innerText = “Mouse is gone!”;}
    title.addEventListner(“mouseleave”, handleMouseLeave);
    -해석: 마우스를 떼면 “Mouse is gone!”이라는 문구로 바뀜.

--- nomad/app.js
+++ nomad/app.js
@@ -1,7 +1,20 @@
 const title = document.querySelector(".hello h1");
 
+console.dir(title);
+
 function handleTitleClick() {
   console.log("title was clicked!");
+  title.style.color = "teal";
+}
+
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
 }
 
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
