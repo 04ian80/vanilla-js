311abb5f7ace247c6afd2ecbf799bf8382091e68
Ain Chung <artnouveau0804@gmail.com>
2022-03-16 22:38:08 +0900

    #3.5 more events
    onclick, oncopy…
    =on은 listen이라는 뜻
    마우스가 title위에 있을 때 event는 [onmouseenter]
    
    function handleMouseEnter(){title.innerText = “mouse is here”;}
    title.addEventListner(“mouseenter”, handleMouseEnter);
    -해석: 함수 handleMouseEnter을 실행하면 inner text가 “mouse is here”이라는 문구로 바뀌어서 출력됨. “mouseenter” event를 실행시키기 위해 title.addEventListner() 괄호 안에 event이름과 함수 이름을 입력해줌.
    
    function handleMouseLeave(){title.innerText = “Mouse is gone!”;}
    title.addEventListner(“mouseleave”, handleMouseLeave);
    -해석: 마우스를 떼면 “Mouse is gone!”이라는 문구로 바뀜.
    click: 클릭
    mouseenter: hover
    mouseleave: not hover
    resize: 화면 사이즈 조정
    copy: 복사
    paste: 붙혀넣기
    offline: 와이파이 연결끊김
    online: 와이파이 연결 됨.
    document.body.style.color = “blue”; == 도큐먼트 바디 색상
    title.style.color = “teal”; == 변수 title의 색상.

--- nomad/app.js
+++ nomad/app.js
@@ -15,6 +15,26 @@ function handleMouseLeave() {
   title.innerText = "Mouse is gone!";
 }
 
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copied!");
+}
+
+function handleWindowOffline() {
+  alert("WIFI disconnected");
+}
+
+function handleWindowOnline() {
+  alert("WIFI connected");
+}
+
 title.addEventListener("click", handleTitleClick);
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
