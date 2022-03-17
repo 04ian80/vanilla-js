995c693e19cc1849cfd937e7f3995b50a4c53980
Ain Chung <artnouveau0804@gmail.com>
2022-03-17 23:18:57 +0900

    #3.6 CSS in JS
    h1을 누를 때마다 색깔이 blue-tomato가 번갈아가면서 바뀌게 하기:
    function handleTitleClick() {
       if(h1.style.color === “blue”){
    	h1.style.color = “tomato”;
       } else {
    	h1.style.color = “blue”;
       }
       h1.style.color = newColor;
    }
    
    해석: h1의 색상이 “blue”일 때 값은 true이며 “tomato”가 실행.
    h1의 색상이 “tomato”일 때 값은 false이기 때문에 else안에 “blue”가 실행
    ->이렇게 하면 h1.style.color가 너무 많아 보기 안좋기 때문에 변수를 만들어줌.
    
    function handleTitleClick() {
       const currentColor = h1.style.color
       let newColor;
       if(currentColor === “blue”){
    	newColor = “tomato”;
       } else {
    	newColor = “blue”;
       }
       h1.style.color = newColor;
    }
    **이해**
    해석:
    1. h1.style.color를 중복적으로 쓰는건 보기에도 안좋고 팀원이 내 코드를 봤을때 쉽게 이해할수 없음.
    2. 그래서 h1.style.color를 “현재 브라우저에 표시되어 있는 색상”currentColor라는 변수로 지정(point)해준 뒤, if()괄호 안에 써준다.(“현재 색상이 ‘blue’라면”이라는 뜻으로 알 수 있게)
    3. 그리고 마우스로 브라우저에서 h1을 클릭할때마다 색깔이 바뀌게 하려면 값이 바뀔 수 있는 변수로 선언 해야 함(let).
    4. 우선 이름 먼저 선언해 주는데, let newColor = h1.style.color로 입력하면
    newColor라는 변수에 h1.style.color이 할당이 되서 newColor = “black”;이 되기 때문에 여기서 끝이 남. 더이상 진행이 안됨.
    5. 그래서 먼저 newColor라는 변수를 먼저 선언해주고(변수를 먼저 선언하지 않으면 조건문에서 newColor를 사용해도 의미가 없음.)newColor = “blue”;처럼 할당해야 한다.
    ex)let a=5; a=3; 이렇게 해야 a의 값이 업데이트가 됨. 만약 a=5; a=3; 이렇게 하면 아무일도 일어나지 않음)
    6. h1에서 이 함수가 발생하게 하려면 조건문 아래에 h1.style.color = newColor를 해줘야 함.
    ->여기서 newColor = h1.style.color라고 하지 않는 이유:
    이 코드는 “newColor에 h1.style.color값을 할당한다”는 의미. 즉 h1.style.color은 “h1”의 디폴트색상, “black”이다. 그러므로 newColor에 “black”이 대입되는데 이렇게 되면 또 4번을 반복하게 된다.
    7. h1.style.color에 newColor를 할당해야 함으로 h1.style.color = newColor를 입력. h1.style.color에 색깔정보가 할당되고 태그에 저장됨 색깔이 업데이트 됨.

--- nomad/app.js
+++ nomad/app.js
@@ -1,40 +1,14 @@
-const title = document.querySelector(".hello h1");
-
-console.dir(title);
+const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  console.log("title was clicked!");
-  title.style.color = "teal";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  title.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copied!");
-}
-
-function handleWindowOffline() {
-  alert("WIFI disconnected");
-}
-
-function handleWindowOnline() {
-  alert("WIFI connected");
-}
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
