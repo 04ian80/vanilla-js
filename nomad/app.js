914e47f8dc62063506452adf736a898c0b61e63f
Ain Chung <artnouveau0804@gmail.com>
2022-03-18 21:18:14 +0900

    #4.3 events (part.2)
    a 태그의 기본동작인 “다른 페이지로 이동”을 막으려면 event.preventDefault를 사용 하면 된다.
    여기서 console.dir(event)를 해보면 a를 눌렀을 때 다른 페이지로 이동 하지 않고 console에 event에 관한 정보들이 출력된다.

--- nomad/app.js
+++ nomad/app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function handleLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
 
 loginForm.addEventListener("submit", handleLoginSubmit);
+link.addEventListener("click", handleLinkClick);
