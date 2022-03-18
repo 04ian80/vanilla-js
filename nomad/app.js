0f543ccfe0bee9f2c4d3bbf195c6943f83e90034
Ain Chung <artnouveau0804@gmail.com>
2022-03-18 22:26:08 +0900

    #4.4 getting username
    로그인 했을 때 form 자체를 없애려면:
    1. css에 class .hidden을 만들어 주고, 속성은 display: none;으로 해준다. 그리고 원할 때 form에 클래스를 더해준다.
    2. 함수 안에 const username = loginInput.value 를 다시 선언해주고
    이 함수를 실행하면 동작 할 수 있게 loginForm.classList.add(“hidden”);을 입력해준다.
    해석: form에 class “hidden”을 추가해준다.
    3. 그리고 맨 아래에 console.log(username);을 입력해서 form을 submit하면 username이 console에 출력되게 한다.
    4. 이렇게 하면 새로고침이 event.preventDefault로 인해 막아지고,
    loginForm.classList.add(“hidden”);로 인해 form이 사라지며,
    console.log(username);로 인해 usename(loginInput.value)이 console에 출력된다.
    5.<h1 class=“hidden”></h1> 태그를 html에 만들어준다. 우리가 만들어야 할 것은 form이 표시되고 있을 때 h1이 표시되지 않고, form이 사라졌을 때는 h1이 표시되게 하는 것:
    1) h1태그에 id #greeting을 만들어 주고, js에서 greeting을 querySelector로 검색해서 변수를 만들어준다.
    2) 그런 다음, event가 발생했을 때 greeting.innerText가 “Hello ” + username으로 뜨게 설정해준다.
    3) 이렇게만 하면 display: none인 상태에서 innerText가 추가되는 것이기 때문에 form이 사라지고 난 뒤 보이는게 없음.
    4) 그래서 함수 안에 greeting.classList.remove(“hidden”);을 해줘서 hidden class가 사라지게 한다.
    5) 여기서 add(“hidden”)이랑 remove(“hidden”)이 중복 됐기 때문에 “hidden”을 변수에 할당해준다. HIDDEN_CLASSNAME으로 해주는데 대문자로 하는 이유는 보통 string만 포함된 변수는 대문자로 표기하고 저장할 때 사용함. 그리고 loginInput이나 loginForm처럼 중요한 정보가 담긴게 아니여서 대문자로 씀.
    6. greeting.innerText = “Hello ” + username; 이 코드를 똑같은 결과이지만 다른 방식으로 쓸 수 있음. 왜냐하면 hello 뒤에 띄어쓰기를 해야하고 플러스를 하는게 귀찮을 수 있기 때문:
    greeting.innerText = `Hello ${username}`;
    `->백틱

--- nomad/app.js
+++ nomad/app.js
@@ -1,16 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function handleLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
-}
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  //greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", handleLoginSubmit);
-link.addEventListener("click", handleLinkClick);
